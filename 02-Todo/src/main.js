2e4f4f4c787cfe70a243a0dedd020a85ee08fad1
anandjos <anandjose5@gmail.com>
2021-05-27 19:26:49 +0530

    [02] Add edit option and change app name

--- 02-Todo/src/main.js
+++ 02-Todo/src/main.js
@@ -1,26 +1,43 @@
 let todoList = [];
-function taskElement(task,id){
-    let taskHTML = `<div id=${id}><input type="text" value=${task} readonly><a onclick="edit(this)"><i class="icon-fixed-width icon-pencil icon-2x"></i></a> <a onclick="remove(this)"><i class="icon-fixed-width icon-trash icon-2x"></i></a></br></div>`;
-    document.getElementById('list').insertAdjacentHTML('beforeend' ,taskHTML);
+function exists(taskname){
+    let exist = false;
+    todoList.forEach(todo=>{
+        if(todo.task==taskname){
+            exist = true;
+            return;
+        }
+    });
+    return exist;
 }
-function add(){
-    var task = document.getElementById('task').value;
-    document.getElementById('task').value = '';
-    if(task==='')
-    {
-        document.getElementById('warning').innerHTML = "Enter task name!";
+function warning(msg){
+    document.getElementById('warning').innerHTML = msg;
         setTimeout(function(){
             document.getElementById("warning").innerHTML = '';
             },900);
+}
+function addTaskElement(task,id){
+    let taskHTML = `<div id=${id}><input type="text" class="ele" value="${task}" readonly><a onclick="edit(this)"><i class="icon-fixed-width icon-pencil icon-2x"></i></a> <a onclick="remove(this)"><i class="icon-fixed-width icon-trash icon-2x"></i></a></br></div>`;
+    document.getElementById('list').insertAdjacentHTML('beforeend' ,taskHTML);
+}
+function add(){
+    var taskname = document.getElementById('task').value;
+    document.getElementById('task').value = '';
+    if(taskname==='')
+    {
+        warning("Enter taskname!");
         return;
     }
+    /*if(exists(taskname)){
+        warning("Task already exists!");
+        return;
+    }*/
     const todo = {
         id: `task${todoList.length}`,
-        task: task,
+        task: taskname,
         isDone: false
     }
     todoList.unshift(todo);
-    taskElement(task,todo.id);
+    addTaskElement(taskname,todo.id);
 }
 function remove(temp){
     let id = temp.parentNode.getAttribute('id');
@@ -28,5 +45,15 @@ function remove(temp){
 }
 function edit(temp){
     let parent = temp.parentNode;
-    parent.children[0].readOnly = false;
+    let child1 = parent.children[0];
+    let child2 = parent.children[1];
+    if(child1.readOnly){
+        child1.readOnly = false;
+        child2.innerHTML = `<a onclick="edit(this)"><i class="icon-li icon-ok icon-2x"></i></a>`;
+    }
+    else{
+        /*'<a><i class="icon-li icon-ok">'*/
+        child1.readOnly = true;
+        child2.innerHTML = `<a onclick="edit(this)"><i class="icon-fixed-width icon-pencil icon-2x"></i></a>`;
+    }
 }
