12f1d8e389e7a83c43a998d3c51a1daf807c3794
anandjos <anandjose5@gmail.com>
2021-06-27 00:27:34 +0530

    [06] Add collision and change styles

--- 06 - Snake Game/src/js/main.js
+++ 06 - Snake Game/src/js/main.js
@@ -12,21 +12,27 @@ function setup() {
   let canvasWidth = window.innerWidth - (window.innerWidth % 20);
   let myCanvas = createCanvas(canvasWidth, canvasHeight);
   myCanvas.parent("grid");
-  xCor = [res * 5, res * 6,res*7,res*8];
-  yCor = [res * 5, res * 5,res*5,res*5];
+  xCor = [res * 5, res * 6,res*7,res*8,res*9,res*10];
+  yCor = [res * 5, res * 5,res*5,res*5,res*5,res*5];
   direction='right';
 }
 function draw() {
   background(0);
   frameRate(10);
+    strokeWeight(10);
   //moveSnake();
   for (i = 0; i < xCor.length - 1; i++) {
-    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
     stroke(255);
-    strokeWeight(10);
+    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
   }
+  stroke('grey');
+  strokeWeight(15);
+  point(xCor[i],yCor[i]);
+  stroke(255,0,0);
+  strokeWeight(10);
   point(foodCor[0],foodCor[1]);
   moveSnake();
+  checkSnakeCollission();
   checkFood();
   //noLoop();
 }
@@ -63,6 +69,17 @@ function moveSnake() {
   }
   }
 }
+function checkSnakeCollission(){
+  const xHead = xCor[xCor.length-1];
+  const yHead = yCor[yCor.length-1];
+  for(let i=0;i<xCor.length-1;i++){
+    if(xCor[i]==xHead&&yCor[i]==yHead){
+      noLoop();
+    lp = false;
+    document.getElementById('score').innerHTML=`GAME OVER! SCORE :${appleCount}`;
+    }
+  }
+}
 function checkFood(){
   if(foodCor[0]==xCor[xCor.length-1]&&foodCor[1]==yCor[yCor.length-1]){
     appleCount++;
@@ -75,6 +92,7 @@ function appleRandom() {
   let x = 10*Math.floor(Math.random() * (width/10 - 2) + 1);
   let y = 10*Math.floor(Math.random() * (height/10 - 2) + 1);
   foodCor = [x,y];
+  stroke(255,0,0);
   point(x, y);
 }
 function play() {
